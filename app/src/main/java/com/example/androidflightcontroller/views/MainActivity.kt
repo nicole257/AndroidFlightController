4687bd045c45e903a8d154243cd79c49c624ec00
Nicole Sharabany <nicolesharabani7@gmail.com>
2021-06-23 16:35:45 +0300

    message for invalid input, including ip regex

--- app/src/main/java/com/example/androidflightcontroller/views/MainActivity.kt
+++ app/src/main/java/com/example/androidflightcontroller/views/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.example.androidflightcontroller.views
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidflightcontroller.R
 import com.example.androidflightcontroller.viewmodels.ControllerViewModel
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private var vm = ControllerViewModel()
+    private lateinit var messageText: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        messageText = findViewById<TextView>(R.id.message)
 
-        findViewById<Button>(R.id.connect_button).setOnClickListener { connectByInput() }
+        findViewById<Button>(R.id.connect_button).setOnClickListener {
+            if (isValidInput()){
+                connectByInput()
+            }
+        }
         val messageText = findViewById<TextView>(R.id.message)
         findViewById<Button>(R.id.disconnect_button).setOnClickListener {
             try{
@@ -84,7 +94,19 @@ class MainActivity : AppCompatActivity() {
         messageText.setTextColor(color)
         messageText.visibility = View.VISIBLE
     }
-    private fun hasValidInput(ip: String, port: String, messageText: TextView): Boolean{
+
+    private fun isValidInput(): Boolean{
+        val ipEditBox = findViewById<EditText>(R.id.ip_box)
+        val ip = ipEditBox.text.toString()
+        val portEditBox = findViewById<EditText>(R.id.port_box)
+        val port = portEditBox.text.toString()
+
+        if (areFieldsNotEmpty(ip, port))
+            if (isValidIPAddress(ip))
+                return true
+        return false
+    }
+    private fun areFieldsNotEmpty(ip: String, port: String): Boolean{
         if (ip.isEmpty() && port.isEmpty()){
             setMessageOn(messageText, getString(R.string.emptyFields), Color.RED)
             return false
@@ -106,7 +128,7 @@ class MainActivity : AppCompatActivity() {
         val port = portEditBox.text.toString()
         val messageText = findViewById<TextView>(R.id.message)
 
-        val valid = hasValidInput(ip, port, messageText)
+        val valid = areFieldsNotEmpty(ip, port)
         if (!valid)
             return
 
@@ -130,6 +152,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun isValidIPAddress(ip: String?): Boolean {
 
+        // Regex for digit from 0 to 255.
+        val zeroTo255 = ("(\\d{1,2}|(0|1)\\"
+                + "d{2}|2[0-4]\\d|25[0-5])")
+
+        // Regex for a digit from 0 to 255 and
+        // followed by a dot, repeat 4 times.
+        // this is the regex to validate an IP address.
+        val regex = (zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255)
+
+        // Compile the ReGex
+        val p: Pattern = Pattern.compile(regex)
+
+        // If the IP address is empty
+        // return false
+        if (ip == null) {
+            return false
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given IP address
+        // and regular expression.
+        val m: Matcher = p.matcher(ip)
+
+        // Return if the IP address
+        // matched the ReGex
+        //return m.matches()
+        if (m.matches())
+            return true
+        setMessageOn(messageText, getString(R.string.invalidIP), Color.RED)
+        return false
+    }
 
 }
